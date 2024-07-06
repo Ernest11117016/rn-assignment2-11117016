d007b23ef097e7461f570f8724d777af48688dc7
Ernest Sintim <ernestsintim23@gmail.com>
2024-07-06 12:49:51 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+//the imports
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+//The main  component
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+      My name is <Text style={styles.boldText}>Ernest Sintim</Text>
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+//the styling
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#236745',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+  boldText: {
+    fontWeight: 'bold'
+  }
+});
